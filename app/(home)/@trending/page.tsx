d4877c626b0c9b0b2affa978967348312cd7b9eb
zer0dt <zer0dt@protonmail.com>
2024-01-22 05:10:29 -0500

    edit trending feed tab check 3

--- app/(home)/@trending/page.tsx
+++ app/(home)/@trending/page.tsx
@@ -178,7 +178,7 @@ interface TrendingFeedProps {
 
 export default async function TrendingFeed({ searchParams }: TrendingFeedProps) {
 
-    const activeTab = searchParams.tab
+    const activeTab = searchParams.tab || "trending"
 
     const activeSort = searchParams.sort || "week";
 
