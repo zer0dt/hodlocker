11f4c4e35395ca1a38f393c27b88676f42e26a9b
remjx <14482423-remjx@users.noreply.gitlab.com>
2024-01-28 08:35:19 -0500

    add github corner icon on large screens

---
+++ app/components/navbar/GithubCorner.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import GitHubCorners from '@uiw/react-github-corners';
+
+export default function GithubCorner() {
+    return (
+        <GitHubCorners
+            position="right"
+            href="https://github.com/zer0dt/hodlocker"
+        />
+    )
+}
