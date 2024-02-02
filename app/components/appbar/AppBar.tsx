3a6e44254623f3bad6347eef3ec19300e323c3ac
remjx <14482423-remjx@users.noreply.gitlab.com>
2024-02-02 11:19:52 -0500

    fix hover area of profile settings button

--- app/components/appbar/AppBar.tsx
+++ app/components/appbar/AppBar.tsx
@@ -308,7 +308,7 @@ const AppBar = () => {
                     </li>
                     <li>
                       <button onClick={() => handleSettingsClick()}
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left">
                         Settings
                       </button>
                     </li>
